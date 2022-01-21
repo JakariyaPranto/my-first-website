b20ea170b383f5296953370083465001c2086d5a
Jakariya Pranto <jakariya9026@gmail.com>
2022-01-22 03:23:11 +0600

    nothing

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-
-function factorial(number) {
-    if(number < 0) {
-        return 'Stupid'
-    } 
-    else if(number == 0) {
-        return '1'
-    } 
-    else {
-        // let x = 1;
-        // for(let i = 1; i <= number; i++) {
-        //     x *= i
-        // }
-        return number * factorial(number - 1)
-    }
+const x = [20,25,15,10,30]
+let y = 0
+for(let i = 0; i < x.length; i++) {
+    y += x[i]
 }
 
-const factNum = 10
-const live = factorial(factNum)
-document.querySelector('h2').innerHTML = `The factorial of ${factNum} is ${live}`
+console.log(y);
