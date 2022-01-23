9d04ad25f8c14e59922124a540eac207538ba1aa
Jakariya Pranto <jakariya9026@gmail.com>
2022-01-24 00:56:23 +0600

    no

--- index.js
+++ index.js
@@ -1,10 +1,61 @@
-const x = "I am      Pranto,  and i am from bangladesh"
-let y = 0;
-for(let i = 0; i < x.length; i++) {
-    let latter = x[i]
-    if(latter == " " && x[i - 1] != " ") {
-        y++
-    }
+//js First Assingment
+/*
+function kmToMiter(num) {
+    return num * 1000
 }
-y++
-console.log(y);
+const result = kmToMiter(5)
+console.log(result);
+*/
+
+//========================================
+
+/*
+function baduget(watch, phone, laptop) {
+    const watchPrise = 100
+    const phonePrise = 200
+    const laptopPrise = 500
+
+    const result = (watch * watchPrise) + (phone * phonePrise) + (laptop * laptopPrise)
+    return result
+}
+
+const maney = baduget(2, 4, 2)
+console.log(maney);
+*/
+
+//=======================================================
+
+/*
+function hotelCost(days) {
+    let cost = 0
+    if(days <= 10) {
+        cost = days * 200
+    }
+    else if(days <= 20) {
+        cost = ((days - 10) * 100) + 2000
+    }
+    else {
+        cost = ((days - 20) * 50) + 3000
+    }
+    return cost
+}
+
+const result = hotelCost(23)
+console.log(result);
+*/
+
+//====================================
+
+function megaFriend(names) {
+    let bigName = ""
+    for(let i = 0; i < names.length; i++) {
+        let fName = names[i]
+        if(fName.length > bigName.length) {
+            bigName = fName
+        }
+    }
+    return bigName
+}
+const friends = ['babu', 'pranto', 'jakariya', 'real', 'arman', 'PrantoBabu']
+const result = megaFriend(friends)
+// console.log(result);
